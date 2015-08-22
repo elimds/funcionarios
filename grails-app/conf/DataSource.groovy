08217d107fc68d3d022e4dc364867224c27990bf
elimds <elimds@gmail.com>
2015-08-22 17:04:50 -0300

    Teste de Datasource com H2 para heroku

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -42,7 +42,7 @@ environments {
             url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
             username = uri.userInfo.split(":")[0]
             password = uri.userInfo.split(":")[1]
-            8?
+            */
         }
     }
 }
